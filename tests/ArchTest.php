b3bc4af8cd79d332ac00155700ca5ceb3f869031
Ruben Van Assche <rubenvanassche@gmail.com>
2026-03-26 15:55:36 +0100

    Skip arch test on PHP < 8.2 where Pest v1 lacks arch testing

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,7 +2,9 @@
 
 use Spatie\OurRay\OurRay;
 
-it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->not->toBeUsed()
-    ->ignoring(OurRay::class);
+if (version_compare(PHP_VERSION, '8.2.0', '>=')) {
+    it('will not use debugging functions')
+        ->expect(['dd', 'dump', 'ray'])
+        ->not->toBeUsed()
+        ->ignoring(OurRay::class);
+}
